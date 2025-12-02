2d75660c9bea459258e3f26c51312ff5d8b05d93
Steven717333 <stevenlee313777@gmail.com>
2025-12-02 18:16:30 +0700

    Commit No. 2 – Building app with coroutines

--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -1,7 +1,30 @@
 package com.example.test_lab_week_12
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.test_lab_week_12.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import androidx.lifecycle.viewModelScope
+import com.example.test_lab_week_12.MovieRepository
+
 
 class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
+    init {
+        fetchPopularMovies()
+    }
+    // define the LiveData
+    val popularMovies: LiveData<List<Movie>>
+        get() = movieRepository.movies
+    val error: LiveData<String>
+        get() = movieRepository.error
+    // fetch movies from the API
+    private fun fetchPopularMovies() {
+// launch a coroutine in viewModelScope
+// Dispatchers.IO means that this coroutine will run on a shared pool of threads
 
+        viewModelScope.launch(Dispatchers.IO) {
+            movieRepository.fetchMovies()
+        }
+    }
 }
