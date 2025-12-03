ece0171f04a51b59a7126fc50d097a9af03a6200
Steven717333 <stevenlee313777@gmail.com>
2025-12-03 16:50:37 +0700

    Commit No. 2 – Upgrading app with flow

--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -7,6 +7,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import androidx.lifecycle.viewModelScope
 import com.example.test_lab_week_12.MovieRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 
 
 class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
@@ -14,17 +17,38 @@ class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel()
         fetchPopularMovies()
     }
     // define the LiveData
-    val popularMovies: LiveData<List<Movie>>
-        get() = movieRepository.movies
-    val error: LiveData<String>
-        get() = movieRepository.error
+//    val popularMovies: LiveData<List<Movie>>
+//        get() = movieRepository.movies
+//    val error: LiveData<String>
+//        get() = movieRepository.error
+//    // fetch movies from the API
+//    private fun fetchPopularMovies() {
+//// launch a coroutine in viewModelScope
+//// Dispatchers.IO means that this coroutine will run on a shared pool of threads
+//
+//        viewModelScope.launch(Dispatchers.IO) {
+//            movieRepository.fetchMovies()
+//        }
+//    }
+
+    private val _popularMovies = MutableStateFlow(
+        emptyList<Movie>()
+    )
+    val popularMovies: StateFlow<List<Movie>> = _popularMovies
+    private val _error = MutableStateFlow("")
+    val error: StateFlow<String> = _error
     // fetch movies from the API
     private fun fetchPopularMovies() {
-// launch a coroutine in viewModelScope
-// Dispatchers.IO means that this coroutine will run on a shared pool of threads
-
         viewModelScope.launch(Dispatchers.IO) {
             movieRepository.fetchMovies()
+                .catch { e ->
+                    // Menangkap exception dari Flow
+                    _error.value = "An exception occurred: ${e.message}"
+                }
+                .collect { movies ->
+                    // Update StateFlow
+                    _popularMovies.value = movies
+                }
         }
     }
 }
